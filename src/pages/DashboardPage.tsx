72a69bf1de2384306f14cd0469b249f61028a349
leirdas <adrielsoh01@gmail.com>
2023-09-13 01:46:35 +0800

    Add users page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -2,17 +2,29 @@ import "./DashboardPage.css";
 import DashboardStats from "@/components/dashboard/statistics/DashboardStats";
 import Sidebar from "@/components/dashboard/sidebar/Sidebar";
 import { useState } from "react";
+import UserList from "@/components/dashboard/users/UserList";
 
 function DashboardPage() {
-  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+  const [showUsers, setShowUsers] = useState(false);
 
   const openSidebar = () => {
-    setOpenSidebarToggle(!openSidebarToggle)
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
+
+  function handleClickUsers() {
+    setShowUsers(!showUsers);
   }
+
   return (
     <div className="dashboard-main">
       <DashboardStats />
-      <Sidebar openSidebarToggle={openSidebarToggle} openSidebar={openSidebar} />
+      <Sidebar
+        openSidebarToggle={openSidebarToggle}
+        openSidebar={openSidebar}
+        handleClickUsers={handleClickUsers}
+      />
+      {showUsers && <UserList />}
     </div>
   );
 }
