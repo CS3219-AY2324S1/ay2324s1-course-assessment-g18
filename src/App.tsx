a3327c85d46b3cf2d52828f5e9c48021aaabd908
senwei01 <leesenwei01@gmail.com>
2023-09-12 18:45:49 +0800

    Did some Side bar changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import DashboardPage from "./pages/DashboardPage";
 import "./App.css";
 
---
+++ src/components/dashboard/sidebar/Sidebar.tsx
@@ -0,0 +1,37 @@
+import 
+{BsCart3, BsGrid1X2Fill, BsPeopleFill, BsFillGearFill}
+ from 'react-icons/bs'
+ import "../../../pages/DashboardPage.css"
+ import "./Sidebar.css"
+ import logo from "../../../assets/dashboard/logo.svg"
+
+function Sidebar({openSidebarToggle, OpenSidebar}) {
+  return (
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
+        <div className='sidebar-title'>
+            <img src={logo} />
+            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+        </div>
+
+        <ul className='sidebar-list'>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsGrid1X2Fill className='icon'/> Dashboard
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsPeopleFill className='icon'/> Users
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGearFill className='icon'/> Setting
+                </a>
+            </li>
+        </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
