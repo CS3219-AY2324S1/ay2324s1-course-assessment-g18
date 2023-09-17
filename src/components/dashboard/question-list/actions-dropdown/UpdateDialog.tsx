316d94d5af3460c5ee9ef3a9e3216ab787842873
Bernice <bernicetoh2003@gmail.com>
2023-09-18 01:40:47 +0800

    Add styling to edit button

--- src/components/dashboard/question-list/actions-dropdown/UpdateDialog.tsx
+++ src/components/dashboard/question-list/actions-dropdown/UpdateDialog.tsx
@@ -12,7 +12,9 @@ function UpdateDialog({ question }: Props) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <div>Edit question</div>
+        <div className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
+          Edit
+        </div>
       </DialogTrigger>
       <CustomDialog dialogTitle="Edit Question">
         <UpdateQuestionForm question={question} setOpen={setOpen} />
