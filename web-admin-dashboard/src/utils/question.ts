1b3410c7acf14114e2a6e8a2edb9b08fcbbde447
senwei01 <leesenwei01@gmail.com>
2023-09-22 11:46:42 +0800

    Changes to the naming of function

--- web-admin-dashboard/src/utils/question.ts
+++ web-admin-dashboard/src/utils/question.ts
@@ -1,6 +1,6 @@
 import { Question } from "@/questionrepo/question.model";
 
-export function areQuestionsEqual(q1: Question, q2: Question): boolean {
+export function areQuestionsContentEqual(q1: Question, q2: Question): boolean {
   return (
     q1.questionDifficulty === q2.questionDifficulty &&
     q1.questionDescription === q2.questionDescription &&
