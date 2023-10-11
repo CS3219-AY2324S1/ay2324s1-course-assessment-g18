43e020aeb1690514a31c6ba7d88a9cfb86ff17b2
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2023-10-11 22:54:31 +0800

    Update Docker files

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +18,7 @@ async function bootstrap() {
   const microservice = app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls:['amqp://localhost:5672'],
+      urls:['amqp://172.17.0.2:5672'],
       queue: 'users_queue',
       queueOptions: {
         durable: false
