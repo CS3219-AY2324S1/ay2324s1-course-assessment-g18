5a88281883635065c273267bf3030178fe20b828
leirdas <adrielsoh01@gmail.com>
2023-10-23 21:54:03 +0800

    Make changes to columns and update imports

--- web-admin-dashboard/src/users/pages/UserDashboardPage.tsx
+++ web-admin-dashboard/src/users/pages/UserDashboardPage.tsx
@@ -1,7 +1,7 @@
-import "./UserDashboardPage.css";
-import UserDashboardStats from "@/users/components/user-dashboard/statistics/UserDashboardStats";
-import HistoryList from "@/users/components/user-dashboard/history-list/HistoryList";
-import FallbackHistoryRepository from "@/users/userHistoryRepo/FallbackHistoryRepository";
+import './UserDashboardPage.css';
+import UserDashboardStats from '@/users/components/user-dashboard/statistics/UserDashboardStats';
+import HistoryList from '@/users/components/user-dashboard/history-list/HistoryList';
+import FallbackHistoryRepository from '@/users/historyRepo/FallbackHistoryRepository';
 
 function UserDashboardPage() {
   return (
