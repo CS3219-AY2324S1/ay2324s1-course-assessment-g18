c3a677d3ea525c2d1537f0d290d0cec153e12bf5
Bernice <bernicetoh2003@gmail.com>
2023-10-24 21:03:28 +0800

    minor change to types

--- web-admin-dashboard/src/users/components/session/chat/ChatBtn.tsx
+++ web-admin-dashboard/src/users/components/session/chat/ChatBtn.tsx
@@ -11,32 +11,36 @@ import { Message } from "@/users/models/message.model";
 import { chatSocket } from "../../match/sockets";
 interface Props {
   peer: User;
-  roomId: String;
+  roomId: string;
 }
 function ChatBtn({ peer, roomId }: Props) {
   const [openChat, setOpenChat] = useState<boolean>(false);
   const messages: Message[] = [];
 
-const [messagesReceived, setMessagesReceived] = useState(messages);
-useEffect(() => {
-    chatSocket.on('sendMessage', (message) => {
-        console.log(message);
-        setMessagesReceived((state : Message[]) => [
-            ...state,
-            {
-              message: message.message,
-              username: message.username,
-            },
-          ]);
+  const [messagesReceived, setMessagesReceived] = useState(messages);
+  useEffect(() => {
+    chatSocket.on("sendMessage", (message) => {
+      console.log(message);
+      setMessagesReceived((state: Message[]) => [
+        ...state,
+        {
+          message: message.message,
+          username: message.username,
+        },
+      ]);
     });
-}, [chatSocket])
-  
+  }, [chatSocket]);
 
   return (
     <div className="flex">
       {openChat && (
         <div className="chat-card">
-          <ChatCard setOpenChat={setOpenChat} peer={peer} roomId={roomId} messagesReceived={messagesReceived}/>
+          <ChatCard
+            setOpenChat={setOpenChat}
+            peer={peer}
+            roomId={roomId}
+            messagesReceived={messagesReceived}
+          />
         </div>
       )}
       <div onClick={() => setOpenChat(!openChat)} className="chat-btn">
