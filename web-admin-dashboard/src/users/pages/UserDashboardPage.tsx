aaeba4b0a560315ea0f4c39ce3dc41a39d36f73e
Bernice <bernicetoh2003@gmail.com>
2023-10-31 21:21:45 +0800

    Create functions to leave queue and session

--- web-admin-dashboard/src/users/pages/UserDashboardPage.tsx
+++ web-admin-dashboard/src/users/pages/UserDashboardPage.tsx
@@ -2,8 +2,32 @@ import "./UserDashboardPage.css";
 import UserDashboardStats from "@/users/components/user-dashboard/statistics/UserDashboardStats";
 import HistoryList from "@/users/components/user-dashboard/history-list/HistoryList";
 import FallbackHistoryRepository from "@/users/userHistoryRepo/FallbackHistoryRepository";
+import { useContext, useEffect } from "react";
+import { matchingSocket } from "../components/match/sockets";
+import { AuthContext } from "@/context/AuthProvider";
 
 function UserDashboardPage() {
+  const { authState } = useContext(AuthContext);
+  const user = authState.userInfo;
+  useEffect(() => {
+    const leaveRoom = () => {
+      if (localStorage.getItem("roomId")) {
+        matchingSocket.emit("leaveSession", {
+          roomId: localStorage.getItem("roomId"),
+        });
+        console.log("left session");
+      }
+    };
+    const leaveQueue = () => {
+      matchingSocket.emit("matchingCancel", {
+        userId: user.username,
+      });
+      console.log("matching cancelled");
+    };
+    leaveRoom();
+    leaveQueue();
+  }, []);
+
   return (
     <div className="user-dashboard-main">
       <UserDashboardStats />
