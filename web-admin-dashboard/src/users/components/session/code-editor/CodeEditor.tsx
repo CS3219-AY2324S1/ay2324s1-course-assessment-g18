e1128928921fdf90355cebb8405eab15cd1af023
leirdas <adrielsoh01@gmail.com>
2023-11-03 00:29:53 +0800

    Pass code changes from code editor to session page

--- web-admin-dashboard/src/users/components/session/code-editor/CodeEditor.tsx
+++ web-admin-dashboard/src/users/components/session/code-editor/CodeEditor.tsx
@@ -1,15 +1,16 @@
-import { CodeEditorProps } from "./interface";
-import Editor from "@monaco-editor/react";
-import { store } from "./store";
-import { useSyncedStore } from "@syncedstore/react";
-import { useState } from "react";
-import { Language } from "@/users/models/language.model";
-import "./CodeEditor.css";
+import { CodeEditorProps } from './interface';
+import Editor from '@monaco-editor/react';
+import { store } from './store';
+import { useSyncedStore } from '@syncedstore/react';
+import { useState } from 'react';
+import { Language } from '@/users/models/language.model';
+import './CodeEditor.css';
 interface Props {
   roomId: string;
   language: Language;
+  onCodeChange: (newCode: string) => void;
 }
-export default function CodeEditor({ roomId, language }: Props) {
+export default function CodeEditor({ roomId, language, onCodeChange }: Props) {
   const state = useSyncedStore(store);
 
   return (
@@ -17,7 +18,13 @@ export default function CodeEditor({ roomId, language }: Props) {
       <Editor
         language={language}
         value={state.codeTextStore[roomId]}
-        onChange={(value) => (state.codeTextStore[roomId] = value)}
+        // onChange={(value) => {
+        //   state.codeTextStore[roomId] = value;
+        // }}
+        onChange={(value) => {
+          onCodeChange(value);
+          state.codeTextStore[roomId] = value;
+        }}
         className="editor"
       />
     </div>
--- web-admin-dashboard/src/users/pages/SessionPage.tsx
+++ web-admin-dashboard/src/users/pages/SessionPage.tsx
@@ -1,37 +1,49 @@
-import React, { useRef, useState } from "react";
-import CodeEditor from "../components/session/code-editor/CodeEditor";
-import LanugageSelect from "../components/form/LanguageSelect";
-import { Language } from "../models/language.model";
-import QuestionView from "../components/session/question-view/QuestionView";
-import { QuestionDifficulty } from "@/questionrepo/question.model";
-import ChatBtn from "../components/session/chat/ChatBtn";
-import { User, UserRole } from "@/userRepo/user.model";
-import { useLocation } from "react-router-dom";
-import { chatSocket, matchingSocket } from "../components/match/sockets";
-import PeerLeftDialog from "../components/session/dialog/PeerLeftDialog";
+import React, { useEffect, useRef, useState } from 'react';
+import CodeEditor from '../components/session/code-editor/CodeEditor';
+import LanugageSelect from '../components/form/LanguageSelect';
+import { Language } from '../models/language.model';
+import QuestionView from '../components/session/question-view/QuestionView';
+import { QuestionDifficulty } from '@/questionrepo/question.model';
+import ChatBtn from '../components/session/chat/ChatBtn';
+import { User, UserRole } from '@/userRepo/user.model';
+import { useLocation } from 'react-router-dom';
+import { chatSocket, matchingSocket } from '../components/match/sockets';
+import PeerLeftDialog from '../components/session/dialog/PeerLeftDialog';
 
 function SessionPage() {
   const [lang, setLang] = useState<Language>(Language.JavaScript);
   const [peerLeft, setPeerLeft] = useState(false);
   const location = useLocation();
-  const tempQn = {
-    questionId: 1,
-    _id: "e0bd7857-17b3-4811-9434-3f623efa78ae",
-    questionTitle: "Two Sum",
-    questionCategories: ["array"],
-    questionDifficulty: QuestionDifficulty.Easy,
-    questionDescription:
-      "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.",
-    questionExamples: [
-      ["nums = [2,7,11,15], target = 9", "[0,1]"],
-      ["nums = [3,2,4], target = 6", "[1,2]"],
-      ["nums = [3,3], target = 6", "[0,1]"],
-    ],
-    questionConstraints: "Hi this is a constraint",
+
+  const [code, setCode] = useState('');
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
   };
 
-  matchingSocket.on("partnerLeaveSession", (payload) => {
-    console.log("partner left the session");
+  useEffect(() => {
+    // console.log('Current code: ', code);
+    setCode(code);
+    console.log('Current code: ', code);
+  }, [code]);
+
+  const tempQn = {
+    questionId: 1,
+    _id: 'e0bd7857-17b3-4811-9434-3f623efa78ae',
+    questionTitle: 'Two Sum',
+    questionCategories: ['array'],
+    questionDifficulty: QuestionDifficulty.Easy,
+    questionDescription:
+      'Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer in any order.',
+    questionExamples: [
+      ['nums = [2,7,11,15], target = 9', '[0,1]'],
+      ['nums = [3,2,4], target = 6', '[1,2]'],
+      ['nums = [3,3], target = 6', '[0,1]'],
+    ],
+    questionConstraints: 'Hi this is a constraint',
+  };
+
+  matchingSocket.on('partnerLeaveSession', (payload) => {
+    console.log('partner left the session');
     setPeerLeft(true);
     //logic to show the dialog that partner left the session. Navigate to user-dashboard in 5 sec or smth.
   });
@@ -47,7 +59,11 @@ function SessionPage() {
         <div className="h-max w-40 pt-5 pb-5 flex ml-5">
           <LanugageSelect setData={setLang} data={lang} />
         </div>
-        <CodeEditor roomId="1" language={lang} />
+        <CodeEditor
+          roomId="1"
+          language={lang}
+          onCodeChange={handleCodeChange}
+        />
         <ChatBtn peer={peer} roomId={location.state.roomId} />
       </div>
       {peerLeft && <PeerLeftDialog peer={peer} />}
