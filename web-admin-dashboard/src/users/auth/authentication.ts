a69ca3c0be909d1b9be19bd311c612905fbd012d
Bernice <bernicetoh2003@gmail.com>
2023-11-05 12:09:35 +0800

    Fix auth bug

--- web-admin-dashboard/src/users/auth/authentication.ts
+++ web-admin-dashboard/src/users/auth/authentication.ts
@@ -1,20 +1,22 @@
 import { User } from "@/userRepo/user.model";
-import axios from "axios";
+import api from "@/utils/api";
 
 export async function updateUsername(userOldEmail: string, username: string) {
+  const res = await api.put(
+    `http://localhost:4000/users/update/${userOldEmail}`,
+    {
+      username,
+    }
+  );
+  const response = res.data as User;
+  return response;
+}
+
+export async function getMyself(email: string) {
   try {
-    const res = await axios.put(
-      `http://localhost:4000/users/update/${userOldEmail}`,
-      {
-        username,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      }
-    );
+    const res = await api.get(`http://localhost:4000/users/getUser/${email}`);
     const response = res.data as User;
+    console.log(response);
     return response;
   } catch (e) {
     console.log(e);
