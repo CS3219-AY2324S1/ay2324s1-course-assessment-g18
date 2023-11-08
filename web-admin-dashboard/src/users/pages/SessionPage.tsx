adb8fb61db7b3828cd4e780644bab97e4657919a
Bernice <bernicetoh2003@gmail.com>
2023-11-08 11:55:10 +0800

    Fix RoomId

--- web-admin-dashboard/src/users/pages/SessionPage.tsx
+++ web-admin-dashboard/src/users/pages/SessionPage.tsx
@@ -15,7 +15,6 @@ import { languageOptions } from "../constants/languageOptions";
 import LanguageSelect from "../components/form/LanguageSelect";
 import api from "@/utils/api";
 
-
 const javascriptDefault = `
 console.log("hello");
 `;
@@ -32,16 +31,16 @@ function SessionPage() {
 
   useEffect(() => {
     console.log(location.state.question);
-  })
-//   useEffect(() => {
-//     setStatus("loading");
-    // api.get(`http://127.0.0.1:4001/questions/random/${location.state.difficulty}`).then((res) => {
-    //     const question = res.data[0]['questionDifficulty'][0];
-    //     console.log(question);
-    //     setQuestion(question);
-    //     setStatus("success");
-    // }).catch((err) => console.log(err));
-//   }, [])
+  });
+  //   useEffect(() => {
+  //     setStatus("loading");
+  // api.get(`http://127.0.0.1:4001/questions/random/${location.state.difficulty}`).then((res) => {
+  //     const question = res.data[0]['questionDifficulty'][0];
+  //     console.log(question);
+  //     setQuestion(question);
+  //     setStatus("success");
+  // }).catch((err) => console.log(err));
+  //   }, [])
   const tempQn = {
     questionId: 1,
     _id: "e0bd7857-17b3-4811-9434-3f623efa78ae",
@@ -65,7 +64,9 @@ function SessionPage() {
   });
 
   function findLanguageByValue(value) {
-    const foundLanguage = languageOptions.find((language) => language.value === value);
+    const foundLanguage = languageOptions.find(
+      (language) => language.value === value
+    );
     return foundLanguage;
   }
 
@@ -74,7 +75,7 @@ function SessionPage() {
     console.log("selected Option...", sl);
     setLanguage(sl);
   };
-  
+
   const onChange = (action, data) => {
     switch (action) {
       case "code": {
@@ -92,21 +93,20 @@ function SessionPage() {
       language_id: language.id,
       // encode source code in base64
       source_code: btoa(code),
-
     };
     console.log("Source Code: ", formData.source_code);
     const options = {
       method: "POST",
-      url: 'https://judge0-ce.p.rapidapi.com/submissions',
+      url: "https://judge0-ce.p.rapidapi.com/submissions",
       params: {
-        base64_encoded: 'true',
-        fields: '*'
+        base64_encoded: "true",
+        fields: "*",
       },
       headers: {
-        'content-type': 'application/json',
-        'Content-Type': 'application/json',
-        'X-RapidAPI-Key': '875b0dafe3msh8f895723e4372eep18f2c1jsn22d2780362eb',
-        'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
+        "content-type": "application/json",
+        "Content-Type": "application/json",
+        "X-RapidAPI-Key": "875b0dafe3msh8f895723e4372eep18f2c1jsn22d2780362eb",
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
       },
       data: formData,
     };
@@ -119,7 +119,7 @@ function SessionPage() {
         checkStatus(token);
       })
       .catch((err) => {
-        let error = err.response ? err.response.data : err;
+        const error = err.response ? err.response.data : err;
         setProcessing(false);
         console.log(error);
       });
@@ -127,20 +127,20 @@ function SessionPage() {
 
   const checkStatus = async (token) => {
     const options = {
-      method: 'GET',
-      url: 'https://judge0-ce.p.rapidapi.com/submissions' + '/' + token,
+      method: "GET",
+      url: "https://judge0-ce.p.rapidapi.com/submissions" + "/" + token,
       params: {
-        base64_encoded: 'true',
-        fields: '*'
+        base64_encoded: "true",
+        fields: "*",
       },
       headers: {
-        'X-RapidAPI-Key': '875b0dafe3msh8f895723e4372eep18f2c1jsn22d2780362eb',
-        'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com'
-      }
+        "X-RapidAPI-Key": "875b0dafe3msh8f895723e4372eep18f2c1jsn22d2780362eb",
+        "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+      },
     };
     try {
-      let response = await axios.request(options);
-      let statusId = response.data.status?.id;
+      const response = await axios.request(options);
+      const statusId = response.data.status?.id;
 
       // Processed - we have a result
       if (statusId === 1 || statusId === 2) {
@@ -173,8 +173,11 @@ function SessionPage() {
     <div className="w-full h-full flex flex-row p-5">
       {/* left side */}
       <div className="h-full w-2/5 flex flex-col mt-5">
-        {location.state.question === undefined ? (<h1>Loading</h1>) : 
-        (<QuestionView question={location.state.question} />)}
+        {location.state.question === undefined ? (
+          <h1>Loading</h1>
+        ) : (
+          <QuestionView question={location.state.question} />
+        )}
       </div>
       {/* right side */}
       <div className="h-full w-3/5 flex flex-col">
@@ -184,19 +187,22 @@ function SessionPage() {
           </div>
           <div className="pr-10">
             <button
-                onClick={handleCompile}
-                disabled={!code}
-                className={classnames(
-                  " z-10  px-4 py-1 hover:shadow transition duration-200 bg-green-400 flex-shrink-0 rounded-lg",
-                  !code ? "opacity-50" : ""
-                )}
-              >
-                {processing ? "Processing..." : "Run"}
+              onClick={handleCompile}
+              disabled={!code}
+              className={classnames(
+                " z-10  px-4 py-1 hover:shadow transition duration-200 bg-green-400 flex-shrink-0 rounded-lg",
+                !code ? "opacity-50" : ""
+              )}
+            >
+              {processing ? "Processing..." : "Run"}
             </button>
           </div>
-
         </div>
-        <CodeEditor roomId="1" language={language.value} onChange={onChange}/>
+        <CodeEditor
+          roomId={location.state.roomId}
+          language={language.value}
+          onChange={onChange}
+        />
         <div className="w-full h-2/5 flex flex-row p-5">
           <div className="h-full w-full flex flex-col pr-5">
             <OutputWindow outputDetails={outputDetails} />
@@ -213,5 +219,3 @@ function SessionPage() {
 }
 
 export default SessionPage;
-
-
