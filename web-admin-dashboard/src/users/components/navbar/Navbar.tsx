6b6d5c70262b132e946c8b76e60352c6af632f8f
Bernice <bernicetoh2003@gmail.com>
2023-11-10 14:34:29 +0800

    Add pipe

--- web-admin-dashboard/src/users/components/navbar/Navbar.tsx
+++ web-admin-dashboard/src/users/components/navbar/Navbar.tsx
@@ -16,7 +16,7 @@ function Navbar() {
   const location = useLocation();
   const pathname = location.pathname;
 
-  const [openDialog, setOpenDialog] = useState(false);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
   const { authState } = useContext(AuthContext);
   const user = authState.userInfo;
 
@@ -31,10 +31,12 @@ function Navbar() {
     <div className="w-screen h-screen flex flex-col">
       <NavigationMenu className="h-13 w-full flex max-w-none justify-between align-center px-5 bg-white pt-[10px] pb-[10px] pl-[25px] pr-[25px]">
         {/* left side */}
-        <NavigationMenuList className="w-full gap-5">
+        <NavigationMenuList className="w-full gap-2">
           <NavigationMenuItem className="flex gap-2 items-center">
             <img src={logo} className="logo" />
+            <div>PeerPrep</div>
           </NavigationMenuItem>
+          <div className="text-slate-300 text-xl text-center mb-1">|</div>
           <NavigationMenuItem className="font-bold flex gap-2 items-center justify-center">
             Hello, {user.username} <p className="text-xl">👋🏻</p>
           </NavigationMenuItem>
@@ -45,7 +47,7 @@ function Navbar() {
             {location.pathname === "/session" ? (
               <EndBtn openDialog={openDialog} setOpenDialog={setOpenDialog} />
             ) : (
-              <MatchBtn openDialog={openDialog} setOpenDialog={setOpenDialog} />
+              <MatchBtn />
             )}
           </NavigationMenuItem>
           <NavigationMenuItem className="">
