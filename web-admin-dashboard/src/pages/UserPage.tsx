254921f7c1b60aefda34fdf33ee9d4c6143a0432
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2023-11-11 00:30:43 +0800

    Update UserPage

--- web-admin-dashboard/src/pages/UserPage.tsx
+++ web-admin-dashboard/src/pages/UserPage.tsx
@@ -1,15 +1,29 @@
 import "./UserPage.css";
 import UserList from "@/components/users/user-list/UserList";
 import { User } from "@/userRepo/user.model";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { UserRepoContext } from "@/context/UserRepoContext";
+import DashboardStats from "@/components/dashboard/statistics/DashboardStats";
+import Sidebar from "@/components/dashboard/sidebar/Sidebar";
+import LiveUserRepository from "@/userRepo/LiveUserRepository";
 
-function UserPage() {
+interface Props {
+  handleClickDashboard: (event: React.MouseEvent) => void;
+  handleClickUser: (event: React.MouseEvent) => void;
+}
+
+function UserPage({ handleClickDashboard, handleClickUser }: Props) {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
   const [data, setData] = useState<User[]>([]);
   const [isChanged, setIsChanged] = useState<boolean>(false);
-  const { userRepo } = useContext(UserRepoContext);
+  const [userRepo, setUserRepo] = useState<LiveUserRepository>(
+    new LiveUserRepository()
+  );
 
+  const openSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
   useEffect(() => {
     async function getDataBackend() {
       const res: User[] = await userRepo.getUsers();
@@ -21,9 +35,18 @@ function UserPage() {
   }, [isChanged, userRepo]);
 
   return (
-    <div className="user-main">
-      <UserList data={data} setIsChanged={setIsChanged} />
-    </div>
+    <UserRepoContext.Provider value={{ userRepo, setUserRepo }}>
+      <div className="user-main">
+        <DashboardStats dataLen={data.length} />
+        <Sidebar
+          openSidebarToggle={openSidebarToggle}
+          openSidebar={openSidebar}
+          handleClickDashboard={handleClickDashboard}
+          handleClickUser={handleClickUser}
+        />
+        <UserList data={data} setIsChanged={setIsChanged} />
+      </div>
+    </UserRepoContext.Provider>
   );
 }
 
