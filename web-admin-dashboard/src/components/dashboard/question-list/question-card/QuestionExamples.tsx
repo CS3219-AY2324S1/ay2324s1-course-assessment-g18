44b120397e52f68bba8628227335be77de252421
Bernice <bernicetoh2003@gmail.com>
2023-11-12 23:59:32 +0800

    Update questionview for admins

--- web-admin-dashboard/src/components/dashboard/question-list/question-card/QuestionExamples.tsx
+++ web-admin-dashboard/src/components/dashboard/question-list/question-card/QuestionExamples.tsx
@@ -2,12 +2,14 @@ interface Props {
   examples: string[][];
 }
 function QuestionExamples({ examples }: Props) {
+  console.log(examples);
   return (
     <div className="flex gap-[10px] flex-col">
       {examples.map((example, i) => (
         <div key={i}>
           <div className="font-bold">Example {i + 1}</div>
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-[15px]">
+            <img src={example[3]} />
             <div>
               Input: <code className="text-slate-500">{example[0]}</code>
             </div>
